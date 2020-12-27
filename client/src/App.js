0890ac42cb88bab8295b38dc41997d151adcbb75
Illya Artemchuk <ilyalinkis@gmail.com>
2020-12-27 13:17:10 -0800

    frontend progress

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+      <a href="/auth/google">Sign In With Google</a>
+    </div>
+  );
+}
+
+export default App;
--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,13 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          // We have a record with the given ID
-          done(null, existingUser);
-        } else {
-          // Create a new user
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
