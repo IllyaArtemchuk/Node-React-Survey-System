ae75740478ad229df228387048f0c0ad2ae89159
Illya Artemchuk <ilyalinkis@gmail.com>
2020-12-30 14:41:19 -0800

    automatic mailing work

---
+++ services/Mailer.js
@@ -0,0 +1,33 @@
+const sendgrid = require("sendgrid");
+const helper = sendgrid.mail;
+const keys = require("../config/keys");
+
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email("emailyproject@gmail.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map((obj) => {
+      return new helper.Email(obj.email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+  }
+}
+
+module.exports = Mailer;
